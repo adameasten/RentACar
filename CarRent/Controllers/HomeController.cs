603dd54145b5dd875ec70caf4bec5de144ac7b0f
Axel Detlofsson <axeldetlof@gmail.com>
2019-01-07 13:58:16 +0100

    Changed controller action to 'home'

--- CarRent/Controllers/HomeController.cs
+++ CarRent/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace CarRent.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Home()
         {
             return View();
         }
